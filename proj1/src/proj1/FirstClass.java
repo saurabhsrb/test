d1fbe1a665b9bc01f15ff8e8923364e3cbff3edf
Saurabh Sharma <60467240+saurabhsrb@users.noreply.github.com>
2020-05-31 23:27:26 +0530

    first commit

---
+++ proj1/src/proj1/FirstClass.java
@@ -0,0 +1,11 @@
+package proj1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for testing");
+
+	}
+
+}
